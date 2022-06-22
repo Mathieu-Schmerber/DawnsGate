c30d91898992549b482a0f181f86e10ed0a82113
Mathieu Schmerber <ms2066@hw.ac.uk>
2022-06-22 22:59:51 +0100

    Passive Item - Arcane ingot

--- Assets/Game/Scripts/Systems/Items/Passive/ArcaneIngot.cs
+++ Assets/Game/Scripts/Systems/Items/Passive/ArcaneIngot.cs
@@ -1,7 +1,35 @@
-﻿namespace Game.Systems.Items.Passive
+﻿using Nawlian.Lib.Utils;
+using System;
+
+namespace Game.Systems.Items.Passive
 {
 	public class ArcaneIngot : ASpecialItem
 	{
+		private Timer _timer;
 
+		public override void OnEquipped(ItemBaseData data, int quality)
+		{
+			base.OnEquipped(data, quality);
+			_timer = new();
+			_timer.Start(_data.Stages[Quality].Duration, true, OnTick);
+		}
+
+		public override void OnUnequipped()
+		{
+			_timer.Stop();
+			base.OnUnequipped();
+		}
+
+		public override void OnUpgrade()
+		{
+			base.OnUpgrade();
+			_timer.Interval = _data.Stages[Quality].Duration;
+		}
+
+		private void OnTick()
+		{
+			if (_entity.CurrentArmor > 0)
+				_entity.CurrentArmor += _entity.MaxArmor * (_data.Stages[Quality].Amount / 100);
+		}
 	}
 }
