bba798e383635344233b732c7f0d6a47acc4b31c
Nawlian <mathieu.schmerber@epitech.eu>
2022-07-25 17:55:56 +0200

    Editor tool: game view simulation

---
+++ Assets/Editor/GameCameraSimulation.cs
@@ -0,0 +1,24 @@
+﻿using Game.Managers;
+using UnityEditor;
+using UnityEngine;
+
+namespace Game
+{
+	public class GameCameraSimulation
+	{
+		[MenuItem("Tools/Game/Simulate game view")]
+		public static void Simulate()
+		{
+			var view = SceneView.lastActiveSceneView;
+
+			if (view != null)
+			{
+				var cam = GameManager.Camera.GetComponentInChildren<Camera>();
+
+				view.orthographic = cam.orthographic;
+				view.AlignViewToObject(cam.transform);
+				view.Repaint();
+			}
+		}
+	}
+}
