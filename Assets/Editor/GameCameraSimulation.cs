4d7ed1e11d3af0ec25ea9d38775ee34af894c6c2
Nawlian <mathieu.schmerber@epitech.eu>
2022-07-29 15:58:08 +0200

    Fix Game view simulation

--- Assets/Editor/GameCameraSimulation.cs
+++ Assets/Editor/GameCameraSimulation.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Managers;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 namespace Game
@@ -9,15 +10,26 @@ namespace Game
 		[MenuItem("Tools/Game/Simulate game view")]
 		public static void Simulate()
 		{
+			bool needBoot = true;
 			var view = SceneView.lastActiveSceneView;
 
 			if (view != null)
 			{
+				var boot = EditorSceneManager.GetSceneByPath(RunManager.RunSettings.BootScenePath);
+				if (!boot.IsValid())
+				{
+					needBoot = false;
+					boot = EditorSceneManager.OpenScene(RunManager.RunSettings.BootScenePath, OpenSceneMode.Additive);
+				}
+
 				var cam = GameManager.Camera.GetComponentInChildren<Camera>();
 
 				view.orthographic = cam.orthographic;
 				view.AlignViewToObject(cam.transform);
 				view.Repaint();
+
+				if (!needBoot)
+					EditorSceneManager.UnloadSceneAsync(boot);
 			}
 		}
 	}
