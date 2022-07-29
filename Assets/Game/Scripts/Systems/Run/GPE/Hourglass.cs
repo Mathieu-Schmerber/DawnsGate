e4ba0c21d0c755309369415f227113abfd8762e8
Nawlian <mathieu.schmerber@epitech.eu>
2022-07-29 15:44:12 +0200

    Game feel hourglass

--- Assets/Game/Scripts/Systems/Run/GPE/Hourglass.cs
+++ Assets/Game/Scripts/Systems/Run/GPE/Hourglass.cs
@@ -1,32 +1,49 @@
 ﻿using Game.Systems.Run.Rooms;
+using Nawlian.Lib.Systems.Animations;
 using Nawlian.Lib.Systems.Interaction;
+using Pixelplacement;
 using UnityEngine;
 
 namespace Game.Systems.Run.GPE
 {
-	public class Hourglass : ATriggerInteractable
+	public class Hourglass : ATriggerInteractable, IAnimationEventListener
 	{
 		[SerializeField] private ARoom _room;
 
 		private bool _used = false;
-		private AudioSource _source;
+		private Animator _gfx;
 
 		public override string InteractionTitle => "Activate the clock";
 
 		private void Awake()
 		{
-			_source = GetComponent<AudioSource>();
+			_gfx = GetComponentInChildren<Animator>();
 		}
 
 		public override void Interact(IInteractionActor actor)
 		{
 			if (_used)
 				return;
-			_source.Play();
-			_room.Activate();
-			actor.UnSuggestInteraction(this);
-			Destroy(gameObject, _source.time);
 			_used = true;
+			actor.UnSuggestInteraction(this);
+			_gfx.SetBool("Interacted", true);
 		}
+
+		protected override void OnSuggesting(IInteractionActor actor)
+		{
+			if (_used)
+				return;
+			base.OnSuggesting(actor);
+		}
+
+		public void OnAnimationEvent(string animationArg)
+		{
+			if (animationArg == "Activate")
+				_room.Activate();
+		}
+
+		public void OnAnimationEnter(AnimatorStateInfo stateInfo){}
+
+		public void OnAnimationExit(AnimatorStateInfo stateInfo){}
 	}
 }
