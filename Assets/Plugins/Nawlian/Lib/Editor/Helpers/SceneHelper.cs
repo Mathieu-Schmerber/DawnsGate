7543cc5c44b5b36643640f9a089bfba4d57322c5
Nawlian <mathieu.schmerber@epitech.eu>
2022-07-31 15:04:52 +0200

    Scene switcher tool upgrade

---
+++ Assets/Plugins/Nawlian/Lib/Editor/Helpers/SceneHelper.cs
@@ -0,0 +1,46 @@
+﻿using System.IO;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Nawlian.Lib.EditorTools.Helpers
+{
+	public static class SceneHelper
+	{
+		public struct SceneInfo
+		{
+			public string Name;
+			public string Path;
+			public int BuildIndex;
+			public bool IsOpen => Scene.IsValid();
+			public Scene Scene;
+
+			public SceneInfo(string name, string path, Scene scene)
+			{
+				Scene = scene;
+				Name = name;
+				Path = path;
+				Scene = scene;
+				BuildIndex = scene.IsValid() ? scene.buildIndex : -1;
+			}
+		} 
+
+		public static SceneInfo[] GetAllSceneInProject()
+		{
+			string[] pathes = Directory.GetFiles(Application.dataPath, "*.unity", SearchOption.AllDirectories);
+
+			return pathes.Select(x => GetSceneAtPath(x)).ToArray();
+		}
+
+		public static SceneInfo[] GetAllBuiltScene() => EditorBuildSettings.scenes.Select(x => GetSceneAtPath(x.path)).ToArray();
+
+		public static SceneInfo GetSceneAtPath(string path)
+		{
+			var scene = EditorSceneManager.GetSceneByPath(path);
+
+			return new SceneInfo(Path.GetFileNameWithoutExtension(path), path, scene);
+		}
+	}
+}
---
+++ Assets/Plugins/Nawlian/Lib/Editor/SceneSwitcher/SceneSwitcher.cs
@@ -0,0 +1,82 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using System.Linq;
+using System.IO;
+using UnityToolbarExtender;
+using Nawlian.Lib.EditorTools.Helpers;
+using Sirenix.Utilities.Editor;
+using System;
+using System.Collections.Generic;
+using Nawlian.Lib.Utils;
+
+namespace Nawlian.Lib.EditorTools.SceneSwitcher
+{
+	[InitializeOnLoad]
+	public class SceneSwitchLeftButton
+	{
+		public struct SceneItem
+		{
+			public string Name => System.IO.Path.GetFileNameWithoutExtension(Path);
+			public string DisplayName => $"{Directory.GetParent(Path).Parent.Name}\\{System.IO.Path.GetFileNameWithoutExtension(Path)}";
+			public string Path { get; set; }
+			public int BuildIndex { get; set; }
+
+			public SceneItem(string path, int buildIndex)
+			{
+				Path = path;
+				BuildIndex = buildIndex;
+			}
+		}
+
+		static SceneSwitchLeftButton()
+		{
+			ToolbarExtender.LeftToolbarGUI.Add(OnToolbarGUI);
+		}
+
+		private static int GetActiveSceneIndex(SceneItem[] array)
+		{
+			string name = EditorSceneManager.GetActiveScene().name;
+
+			for (int i = 0; i < array.Length; i++)
+				if (array[i].Name == name)
+					return i;
+			return 0;
+		}
+
+		private static void OnToolbarGUI()
+		{
+			SceneItem[] scenes = SceneHelper.GetAllSceneInProject().Select(x => new SceneItem(x.Path, x.BuildIndex)).OrderBy(x => x.DisplayName).ToArray();
+
+			GUILayout.FlexibleSpace();
+
+			int currentScene = GetActiveSceneIndex(scenes);
+			int selected = currentScene;
+
+			selected = EditorGUILayout.Popup(selected, scenes.Select(x => x.DisplayName).ToArray());
+
+			if (selected != currentScene)
+			{
+				if (Application.isPlaying)
+					return;
+
+				OpenSceneMode method;
+				int choice = EditorUtility.DisplayDialogComplex("Scene switcher", $"How do you want to load {scenes[selected].Name} ?", "Change scene", "Cancel", "Add scene");
+
+				switch (choice)
+				{
+					case 0:
+						method = OpenSceneMode.Single;
+						break;
+					case 2:
+						method = OpenSceneMode.Additive;
+						break;
+					default:
+						return;
+				}
+				EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+				EditorSceneManager.OpenScene(scenes[selected].Path, method);
+			}
+		}
+	}
+}
