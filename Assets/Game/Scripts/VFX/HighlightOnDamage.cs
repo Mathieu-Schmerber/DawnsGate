7ee5ebea098f10dd4f43d6bc7c9246eedfed9875
Mathieu Schmerber <mathieu.schmerber@epitech.eu>
2022-08-19 17:38:53 +0200

    White golems fix

--- Assets/Game/Scripts/VFX/HighlightOnDamage.cs
+++ Assets/Game/Scripts/VFX/HighlightOnDamage.cs
@@ -11,12 +11,14 @@ namespace Game.VFX
 		[SerializeField] private Material _highlight;
 		[SerializeField] private float _highlightTime;
 
+		private Material[] _baseMaterials;
 		private Material[] _priorMaterials;
 		private Renderer _renderer;
 		private Damageable _damageable;
 
 		private void OnEnable()
 		{
+			_renderer.materials = _baseMaterials;
 			_damageable.OnDamaged += OnDamageDealt;
 		}
 
@@ -29,6 +31,7 @@ namespace Game.VFX
 		{
 			_renderer = GetComponentInChildren<Renderer>();
 			_damageable = GetComponent<Damageable>();
+			_baseMaterials = _renderer.materials;
 		}
 
 		private IEnumerator Highlight()
